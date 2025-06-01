95fed969e402d321f667d3661d14b3de3f335080
Stefan Müller zzeroo <co@zzeroo.com>
2025-06-01 18:02:18 +0200

    Migration, Model, Forms for MeterTypes

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,73 @@
 mod migrations;
 
 use askama::Template;
+use cot::{reverse_redirect, App, AppBuilder, BoxedHandler, Project};
 use cot::cli::CliMetadata;
+use cot::db::{Auto, Model, model};
 use cot::db::migrations::SyncDynMigration;
+use cot::db::query;
+use cot::form::Form;
 use cot::html::Html;
-use cot::middleware::{AuthMiddleware, LiveReloadMiddleware, SessionMiddleware};
-use cot::project::{MiddlewareContext, RegisterAppsContext, RootHandlerBuilder};
-use cot::router::{Route, Router};
-use cot::static_files::{StaticFile, StaticFilesMiddleware};
-use cot::{App, AppBuilder, BoxedHandler, Project, static_files};
-use cot::db::{model, Auto, LimitedString};
+use cot::middleware::{AuthMiddleware, SessionMiddleware, LiveReloadMiddleware};
+use cot::project::{MiddlewareContext, RegisterAppsContext};
+use cot::request::extractors::Path;
+use cot::request::extractors::RequestDb;
+use cot::request::extractors::RequestForm;
+use cot::response::Response;
+use cot::router::{Route, Router, Urls};
+use cot::static_files;
+use cot::static_files::StaticFile;
+use cot::static_files::StaticFilesMiddleware;
 
-//#[model]
-pub struct MeterType<'a> {
-    //#[model(primary_key)]
-    //id: Auto<i64>,
+#[derive(Debug, Clone)]
+#[model]
+pub struct MeterType {
+    #[model(primary_key)]
+    id: Auto<i64>,
     //#[model(unique)]
     //name: LimitedString<32>,
-    name: &'a str,
+    name: String,
 }
 
-#[derive(Template)]
+#[derive(Debug, Template)]
 #[template(path = "index.html")]
 struct IndexTemplate<'a> {
-    meter_types: Vec<MeterType<'a>>,
+    urls: &'a Urls,
+    meter_types: Vec<MeterType>,
 }
 
-async fn index() -> cot::Result<Html> {
-    let meter_types = vec![
-        MeterType { name: "gas meter" },
-        MeterType { name: "water meter" },
-        MeterType { name: "electricity meter" },
-    ];
-
-    let index_template = IndexTemplate { meter_types };
+async fn index(urls: Urls, RequestDb(db): RequestDb) -> cot::Result<Html> {
+    let meter_types = MeterType::objects().all(&db).await?;
+    let index_template = IndexTemplate {
+        urls: &urls,
+        meter_types
+    };
     let rendered = index_template.render()?;
 
     Ok(Html::new(rendered))
+}
+
+#[derive(Debug, Form)]
+struct MeterTypeForm {
+    #[form(opt(max_length = 100))]
+    name: String,
+}
+
+async fn add_meter_type(urls: Urls, RequestDb(db): RequestDb, RequestForm(meter_type_form): RequestForm<MeterTypeForm>) -> cot::Result<Response> {
+    let meter_type_form = meter_type_form.unwrap();
+
+    MeterType {
+        id: Auto::auto(),
+        name: meter_type_form.name,
+    }.save(&db).await?;
+
+    Ok(reverse_redirect!(urls, "index")?)
+}
+
+async fn remove_meter_type(urls: Urls, RequestDb(db): RequestDb, Path(meter_type_id): Path<i64>) -> cot::Result<Response> {
+    query!(MeterType, $id == meter_type_id).delete(&db).await?;
+
+    Ok(reverse_redirect!(urls, "index")?)
 }
 
 struct CotMeterApp;
@@ -52,7 +83,11 @@ impl App for CotMeterApp {
     }
 
     fn router(&self) -> Router {
-        Router::with_urls([Route::with_handler_and_name("/", index, "index")])
+        Router::with_urls([
+            Route::with_handler_and_name("/", index, "index"),
+            Route::with_handler_and_name("/meter_types/add", add_meter_type, "add-meter-type"),
+            Route::with_handler_and_name("/meter_types/{meter_type_id}/remove", remove_meter_type, "remove-meter-type"),
+        ])
     }
 
     fn static_files(&self) -> Vec<StaticFile> {
@@ -74,7 +109,7 @@ impl Project for CotMeterProject {
 
     fn middlewares(
         &self,
-        handler: RootHandlerBuilder,
+        handler: cot::project::RootHandlerBuilder,
         context: &MiddlewareContext,
     ) -> BoxedHandler {
         handler
